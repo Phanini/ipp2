31b26704b594100ef40ec3afd7cb20d194aab412
kubazabava <jakubpha@protonmail.com>
2023-04-14 17:51:44 +0200

    Corrected errors codes, added check_arg_num, started defvar

--- main.py
+++ main.py
@@ -9,6 +9,53 @@ move() symb/var
 
 """
 
+
+def argument_parse(src, inp):
+    parser = argparse.ArgumentParser(description='interpret.py ')
+    parser.add_argument('--source=', action='store', dest='src', nargs='?')
+    parser.add_argument('--input=', action='store', dest='inp', nargs='?')
+    arguments = parser.parse_args()
+    if arguments.inp is None and arguments.src is None:
+        print("ERROR 10: Wrong arguments given")
+        exit(10)
+    return arguments.src, arguments.inp
+
+
+def xml_check(root):
+    print('-------XML CHECK-------')
+    # Check if program tag exists
+    if root.tag != 'program': return sys.exit('program tag error')
+
+    # Check language attribute
+    if root.attrib.get('language') is None or root.attrib.get('language').lower() != "ippcode23":
+        return sys.exit("Language error")
+
+    # Iterate children of root
+    currentOrder = 0
+    for instruct in root:
+        # Check if tag is instruction
+        if instruct.tag != 'instruction': error_exit(31, "Error 31: Wrong XML format - instruction tag")
+
+        # Check order of instructions
+        currentOrder = int(instruct.get('order')) if int(instruct.get('order')) > currentOrder else \
+            error_exit(32, 'Error 32: Wrong XML instruction order')
+
+        instruction_attribs = list(instruct.attrib.keys())
+        # Check if both order and opcode exist
+        if not ('order' in instruction_attribs) or not ('opcode' in instruction_attribs):
+            error_exit(31, 'Error 31: Wrong XML file format (order/opcode)')
+
+        # Check if type attribute exists
+        for argum in instruct:
+            if 'type' not in argum.attrib.keys(): error_exit(31, "Error 31: Wrong XML file format (no type in arg)")
+    print('----------OK----------')
+
+
+def error_exit(err_num, err_msg):
+    sys.stderr.write(err_msg + '\n')
+    exit(err_num)
+
+
 class memory:
     frames = {
         'GF': dict(),
@@ -35,11 +82,14 @@ class instruction:
     def get_args(self):
         return self.args
 
+    def check_arg_num(self, num):
+        if num != len(self.args):
+            error_exit(56, "Error 56: Wrong number of arguments")
+
     def check_frame(self, mem_frame):
         if (mem_frame == 'TF' and memory.frames['TF'] is None) or \
                 (mem_frame == 'LF' and not memory.frames['LF']):
-            sys.stderr.write("Memory frame doesn't exist")
-            exit(55)
+            error_exit(55, "Error 55: Memory frame doesn't exist")
 
     def set_var_frame(self, frame, var_name, symb):
         if frame in ('GF', 'TF'):
@@ -47,19 +97,19 @@ class instruction:
         elif frame == 'LF':
             memory.frames['LF'][-1][var_name] = symb
         else:
-            sys.stderr.write("Non-existent FRAME")
-            exit()
+            error_exit(55, "Error 55: Frame does not exist")
 
+    # Checks existance of variable and frame
     def check_var(self, mem_frame, var_name) -> bool:
         self.check_frame(mem_frame)
-        match var_name:
+        match mem_frame:
             case 'GF':
                 return var_name in memory.frames['GF'].keys()
             case 'LF':
                 return var_name in memory.frames['LF'].pop().keys()
             case 'TF':
                 return var_name in memory.frames['TF'].keys()
-        return False
+        error_exit(54, "Error 54: variable does not exist")
 
     def move(self):
         var = self.get_args()[0].value
@@ -70,12 +120,14 @@ class instruction:
 
     def write(self):
         result = self.symb_value(self.get_args()[0].value)
-        print('WRITE RESULT: ' + result)
+        print(self.get_args()[0].value)
+        #print('WRITE RESULT: ' + result)
 
     def symb_value(self, value):
         prefix, suffix = value.split("@", 1)
-        print('PREF: '+prefix+' SUF: '+suffix)
+        print('PREF: ' + prefix + ' SUF: ' + suffix)
         if prefix not in ('GF', 'LF', 'TF'):
+            print("NOT GFLFTF")
             return suffix
         match prefix:
             case 'TF':
@@ -86,6 +138,10 @@ class instruction:
                 return memory.frames['LF'][-1][suffix]
         sys.stderr.write('Wrong symb')
         exit()
+
+    def defvar(self):
+        mem_frame, var_name = self.get_args().pop().split("@", 1)
+        self.check_var(mem_frame, var_name)
 
 
     def createframe(self):
@@ -107,8 +163,9 @@ class instruction:
     def instr_switch(self):
         match self.opcode:
             case 'MOVE':  # <var>  <=  <symb>
+                self.check_arg_num(2)
                 self.move()
-            case 'WRITE': # <symb>
+            case 'WRITE':  # <symb>
                 self.write()
             case 'CREATEFRAME':
                 self.createframe()
@@ -116,47 +173,6 @@ class instruction:
                 self.pushframe()
             case 'POPFRAME':
                 self.popframe()
-
-
-def argument_parse(src, inp):
-    parser = argparse.ArgumentParser(description='interpret.py ')
-    parser.add_argument('--source=', action='store', dest='src', nargs='?')
-    parser.add_argument('--input=', action='store', dest='inp', nargs='?')
-    arguments = parser.parse_args()
-    if arguments.inp is None and arguments.src is None:
-        print("ERROR 10: Wrong arguments given")
-        exit(10)
-    return arguments.src, arguments.inp
-
-
-def xml_check(root):
-    print('-------XML CHECK-------')
-    # Check if program tag exists
-    if root.tag != 'program': return sys.exit('program tag error')
-
-    # Check language attribute
-    if root.attrib.get('language') is None or root.attrib.get('language').lower() != "ippcode23":
-        return sys.exit("Language error")
-
-    # Iterate children of root
-    currentOrder = 0
-    for instruct in root:
-        # Check if tag is instruction
-        if instruct.tag != 'instruction': exit(31)
-
-        # Check order of instructions
-        currentOrder = int(instruct.get('order')) if int(instruct.get('order')) > currentOrder else sys.exit(
-            'wrong instruction order')
-
-        instruction_attribs = list(instruct.attrib.keys())
-        # Check if both order and opcode exist
-        if not ('order' in instruction_attribs) or not ('opcode' in instruction_attribs): sys.exit(
-            'child order/opcode error')
-
-        # Check if type attribute exists
-        for argum in instruct:
-            if 'type' not in argum.attrib.keys(): sys.exit("No type in argum param")
-    print('----------OK----------')
 
 
 def main():
@@ -169,8 +185,21 @@ def main():
     # print(argument[1])  # --input
 
     #  handle input and source
-    source_handle = open(argument[0], 'r') if argument[0] is not None else sys.stdin
-    input_handle = open(argument[1], 'r') if argument[1] is not None else sys.stdin
+    if argument[0] is not None:
+        try:
+            source_handle = open(argument[0], 'r')
+        except FileNotFoundError:
+            error_exit(11, "Error 11: File does not exist")
+    else:
+        source_handle = sys.stdin
+
+    if argument[1] is not None:
+        try:
+            input_handle = open(argument[0], 'r')
+        except FileNotFoundError:
+            error_exit(11, "Error 11: File does not exist")
+    else:
+        input_handle = sys.stdin
 
     try:
         tree = ET.parse(source_handle)
