a215637eb1aacd0f5ee927371a0d5fcf0618bd27
kubazabava <jakubpha@protonmail.com>
2023-04-14 23:35:20 +0200

    Added call(), return(), pushs(), pops()

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def argument_parse(src, inp):
 
 
 def xml_check(root):
-    # print('-------XML CHECK-------')
     # Check if program tag exists
     if root.tag != 'program': return sys.exit('program tag error')
 
@@ -48,7 +47,6 @@ def xml_check(root):
         # Check if type attribute exists
         for argum in instruct:
             if 'type' not in argum.attrib.keys(): error_exit(31, "Error 31: Wrong XML file format (no type in arg)")
-    print('=XML OK=')
 
 
 def error_exit(err_num, err_msg):
@@ -56,28 +54,32 @@ def error_exit(err_num, err_msg):
     exit(err_num)
 
 
-class memory:
+class Memory:
     frames = {
         'GF': dict(),
         'LF': list(),
         'TF': None
     }
+    labels = dict()
+    instruction_stack = []
+    program_counter = 1
+    data_stack = []
 
 
-class variable:
+class Variable:
     def __init__(self, arg_type, value):
         self.type = arg_type
         self.value = value
 
 
-class instruction:
+class Instruction:
     def __init__(self, order, opcode):
         self.order = order
         self.opcode = opcode
         self.args = []
 
     def add_argument(self, arg_type, value):
-        self.args.append(variable(arg_type, value))
+        self.args.append(Variable(arg_type, value))
 
     def get_args(self):
         return self.args
@@ -86,28 +88,28 @@ class instruction:
         if num != len(self.args):
             error_exit(56, "Error 56: Wrong number of arguments")
 
-    def check_frame(self, mem_frame): # memory.frames[tf] is None
-        if mem_frame == 'TF' and memory.frames['TF'] is None:
+    def check_frame(self, mem_frame):  # Memory.frames[tf] is None
+        if mem_frame == 'TF' and Memory.frames['TF'] is None:
             error_exit(55, "TF Error 55: Memory frame doesn't exist")
-        if mem_frame == 'LF' and not memory.frames['LF']:
+        if mem_frame == 'LF' and not Memory.frames['LF']:
             error_exit(55, "LF Error 55: Memory frame doesn't exist")
 
-    # Checks existence of variable and frame
-    def check_var(self, mem_frame, var_name) -> bool:
+    # Checks existence of Variable and frame
+    def check_var_exists(self, mem_frame, var_name) -> bool:
         self.check_frame(mem_frame)
         match mem_frame:
             case 'GF' | 'TF':
-                return var_name in memory.frames[mem_frame].keys()
+                return var_name in Memory.frames[mem_frame].keys()
             case 'LF':
-                return var_name in memory.frames['LF'][-1].keys()
+                return var_name in Memory.frames['LF'][-1].keys()
         return False
-        # error_exit(54, "Error 54: variable does not exist")
+        # error_exit(54, "Error 54: Variable does not exist")
 
     def set_var_frame(self, frame, var_name, symb):
         if frame in ('GF', 'TF'):
-            memory.frames[frame][var_name] = symb
+            Memory.frames[frame][var_name] = symb
         elif frame == 'LF':
-            memory.frames['LF'][-1][var_name] = symb
+            Memory.frames['LF'][-1][var_name] = symb
         else:
             error_exit(55, "Error 55: Frame does not exist")
 
@@ -120,7 +122,7 @@ class instruction:
 
     def write(self):
         result = self.symb_value(self.get_args()[0].value)
-        print('WRITE RESULT: ' + result)
+        print(result)
 
     def get_var_value(self, var):
         type, value = var.split('@', 1)
@@ -131,64 +133,118 @@ class instruction:
         # print('PREF: ' + prefix + ' SUF: ' + suffix)
         if prefix not in ('GF', 'LF', 'TF'):
             return suffix
-        if self.check_var(prefix, suffix) is False: error_exit(54, "Error 54: Non-existent var")
+        if self.check_var_exists(prefix, suffix) is False: error_exit(54, "Error 54: Non-existent var")
 
         match prefix:
             case 'GF' | 'TF':
-                return self.get_var_value(memory.frames[prefix][suffix])
+                return self.get_var_value(Memory.frames[prefix][suffix])
             case 'LF':
-                return self.get_var_value(memory.frames['LF'][-1][suffix])
+                return self.get_var_value(Memory.frames['LF'][-1][suffix])
         sys.stderr.write('Wrong symb')
         exit()
 
     def defvar(self):
         mem_frame, var_name = self.get_args()[0].value.split("@", 1)
-        if self.check_var(mem_frame, var_name):
+        if self.check_var_exists(mem_frame, var_name):
             error_exit(52, "Error 52: Variable re-definition")
 
         # Init of an empty <var>
-        var = {var_name: variable(None, None)}
+        var = {var_name: Variable(None, None)}
 
         match mem_frame:
             case 'GF' | 'TF':
-                memory.frames[mem_frame].update(var)
+                Memory.frames[mem_frame].update(var)
             case 'LF':
-                memory.frames['LF'][-1].update(var)
+                Memory.frames['LF'][-1].update(var)
             case _:
                 error_exit(1, "Error ??: DEFVAR: Unknown var")
 
     def createframe(self):
-        memory.frames['TF'] = dict()
+        Memory.frames['TF'] = dict()
 
     def pushframe(self):
-        if memory.frames['TF'] is None:
-            sys.stderr.write("No created frame to PUSHFRAME")
-            exit(55)
-        memory.frames['LF'].append(memory.frames['TF'])
-        memory.frames['TF'] = None
+        if Memory.frames['TF'] is None:
+            error_exit(55, "Error 55: No frame to push")
+        Memory.frames['LF'].append(Memory.frames['TF'])
+        Memory.frames['TF'] = None
 
     def popframe(self):
-        if not memory.frames['LF']:
-            sys.stderr.write("No pushed TF to POP")
-            exit(55)
-        memory.frames['TF'] = memory.frames['LF'].pop()
+        if not Memory.frames['LF']:
+            error_exit(55, "Error 55: No frame to pop")
+        Memory.frames['TF'] = Memory.frames['LF'].pop()
+
+    # TODO: Pochopit to
+    def call(self):
+        label = self.get_args()[0]
+        if label not in Memory.labels.keys(): error_exit(52, "Error 52: Undefined label")
+        Memory.instruction_stack.append(Memory.program_counter)
+        Memory.program_counter = Memory.labels[label];
+
+    def return_ins(self):
+        if not Memory.instruction_stack: error_exit(56, "Error 56: Missing value on instruction stack")
+        Memory.program_counter = Memory.instruction_stack.pop()
+
+    def pushs(self):
+        value = self.get_args()[0].value
+        Memory.data_stack.append(value)
+        # print(Memory.__dict__)
+
+    def pops(self):
+        if not Memory.data_stack: error_exit(56, "Error 56: Pops from empty stack")
+        var = self.get_args()[0].value
+        mem_frame, var_name = var.split('@', 1)
+        value = Memory.data_stack.pop()
+        self.set_var_frame(mem_frame, var_name, value)
+        # print(Memory.__dict__)
+
+    def add(self):
+        var = self.get_args()[0]
+        symb1 = self.get_args()[1]
+        symb2 = self.get_args()[2]
+        
 
     def instr_switch(self):
         match self.opcode:
+            # INSTRUCTIONS FOR FRAMES, CALLS
             case 'MOVE':  # <var>  <=  <symb>
                 self.check_arg_num(2)
                 self.move()
-            case 'WRITE':  # <symb>
-                self.write()
+            case 'CREATEFRAME':
+                self.check_arg_num(0)
+                self.createframe()
+            case 'PUSHFRAME':
+                self.check_arg_num(0)
+                self.pushframe()
+            case 'POPFRAME':
+                self.check_arg_num(0)
+                self.popframe()
             case 'DEFVAR':  # <var>
                 self.check_arg_num(1)
                 self.defvar()
-            case 'CREATEFRAME':
-                self.createframe()
-            case 'PUSHFRAME':
-                self.pushframe()
-            case 'POPFRAME':
-                self.popframe()
+            case 'CALL':  # <label>
+                self.check_arg_num(1)
+                self.call()
+            case 'RETURN':
+                self.check_arg_num(0)
+                self.return_ins()
+
+            # INSTRUCTIONS FOR DATA STACK
+            case 'PUSHS':
+                self.check_arg_num(1)
+                self.pushs()
+            case 'POPS':
+                self.check_arg_num(1)
+                self.pops()
+
+            # INSTRUCTION FOR ARITHMETIC, RELATIONAL, BOOLEAN AND CONVERSION
+            case 'ADD':
+                self.check_arg_num(3)
+                self.add()
+
+            # INSTRUCTION FOR I/O
+            case 'WRITE':  # <symb>
+                self.check_arg_num(1)
+                self.write()
 
 
 def main():
@@ -228,7 +284,7 @@ def main():
 
     # iterate instructions
     for instruct in root:  # <instruction order= opcode=>
-        instruct_tmp = instruction(instruct.get('order'), instruct.get('opcode'))
+        instruct_tmp = Instruction(instruct.get('order'), instruct.get('opcode'))
 
         # Create instruction
         for argum in instruct:  # <arg type= >text
@@ -236,7 +292,7 @@ def main():
         instruction_list.append(instruct_tmp)
 
     for instruct in instruction_list:
-        instruction.instr_switch(instruct)
+        Instruction.instr_switch(instruct)
 
 
 if __name__ == '__main__':
